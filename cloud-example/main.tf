54373b752fe6af52176025a8bb51a9592e861b17
croguerrero <christianmarcelog@gmail.com>
2022-04-11 13:14:53 -0500

    add eksaws

--- cloud-example/main.tf
+++ cloud-example/main.tf
@@ -11,17 +11,14 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "app_server" {
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
- # subnet_ids         = [aws_subnet.example.id]
-  #transit_gateway_id = aws_ec2_transit_gateway.example.id
-  #vpc_id = aws_vpc.example.id
 
   tags = {
-    Name = "AppServerInstance"
+    Name = var.name
   }
 }
